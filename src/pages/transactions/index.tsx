db1624454ad295a771608836173fe106cae206bd
Leonardo de Castilho <leonardo.smendescastilho@gmail.com>
2024-12-19 11:50:45 -0300

    feat(component): add table and style into it

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,13 +1,40 @@
 import Header from '../../components/Header';
 import Summary from '../../components/Summary';
-import { TransactionsContainer } from './styles';
+import {
+	PriceHighlight,
+	TransactionsContainer,
+	TransactionsTable,
+} from './styles';
 
 function Transactions() {
 	return (
-		<TransactionsContainer>
+		<div>
 			<Header />
 			<Summary />
-		</TransactionsContainer>
+			<TransactionsContainer>
+				<TransactionsTable>
+					<tbody>
+						<tr>
+							<td width={'50%'}>Desenvolvimento de site</td>
+							<td>
+								<PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+							</td>
+
+							<td>Venda</td>
+							<td>13/04/2022</td>
+						</tr>
+						<tr>
+							<td>Hamburguer</td>
+							<td>
+								<PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight>
+							</td>
+							<td>Alimentação</td>
+							<td>10/04/2022</td>
+						</tr>
+					</tbody>
+				</TransactionsTable>
+			</TransactionsContainer>
+		</div>
 	);
 }
 
