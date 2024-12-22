12616e4be64a7feac9e328b364040571100be699
Leonardo de Castilho <leonardo.smendescastilho@gmail.com>
2024-12-21 21:51:19 -0300

    feat(component): add Dialog component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
+import * as Dialog from '@radix-ui/react-dialog';
 import logoImg from '../../assets/logo.svg';
+import NewTransactionModal from '../NewTransactionModal';
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './style';
 
 function Header() {
@@ -6,7 +8,14 @@ function Header() {
 		<HeaderContainer>
 			<HeaderContent>
 				<img src={logoImg} />
-				<NewTransactionButton>Nova Transação</NewTransactionButton>
+
+				<Dialog.Root>
+					<Dialog.Trigger asChild>
+						<NewTransactionButton>Nova Transação</NewTransactionButton>
+					</Dialog.Trigger>
+
+					<NewTransactionModal />
+				</Dialog.Root>
 			</HeaderContent>
 		</HeaderContainer>
 	);
