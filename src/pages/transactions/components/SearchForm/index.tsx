a7c645ea2e4828201a18daa372c22243359b016c
Leonardo de Castilho <leonardo.smendescastilho@gmail.com>
2024-12-29 17:12:03 -0300

    feat: create a useForm and schema from input form

--- src/pages/transactions/components/SearchForm/index.tsx
+++ src/pages/transactions/components/SearchForm/index.tsx
@@ -1,14 +1,37 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import { MagnifyingGlass } from 'phosphor-react';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
 import { SearchFormContainer } from './styles';
 
+const searchFormSchema = z.object({
+	query: z.string(),
+});
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>;
+
 function SearchForm() {
+	const { register, handleSubmit, formState } = useForm<SearchFormInputs>({
+		resolver: zodResolver(searchFormSchema),
+	});
+	const { isSubmitting } = formState;
+
+	const handleSearchTransaction = async (data: SearchFormInputs) => {
+		await new Promise((resolve) => setTimeout(resolve, 2000));
+		console.log(data.query);
+	};
+
 	return (
-		<SearchFormContainer>
+		<SearchFormContainer onSubmit={handleSubmit(handleSearchTransaction)}>
 			<input
 				type="text"
 				placeholder="Busque por transações"
+				{...register('query')}
 			/>
-			<button type="submit">
+			<button
+				type="submit"
+				disabled={isSubmitting}
+			>
 				<MagnifyingGlass size={20} />
 				Buscar
 			</button>
